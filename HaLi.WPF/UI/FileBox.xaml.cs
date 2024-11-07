855f41b733085d931bc4aa0589b943a036aa0481
Kwong Hau Lim <haulim.kwong@gmail.com>
2024-11-07 15:51:43 +0800

    UI: FileBox

---
+++ HaLi.WPF/UI/FileBox.xaml.cs
@@ -0,0 +1,139 @@
+﻿using HandyControl.Data;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace HaLi.WPF.UI
+{
+    /// <summary>
+    /// Interaction logic for FileBox.xaml
+    /// </summary>
+    public partial class FileBox : LabeledUI
+    {
+        public string Path
+        {
+            get { return (string)GetValue(PathProperty); }
+            set { SetValue(PathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Path.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PathProperty =
+            DependencyProperty.Register("Path", typeof(string), typeof(FileBox), new PropertyMetadata(string.Empty));
+
+        public bool ShowFullPath
+        {
+            get { return (bool)GetValue(ShowFullPathProperty); }
+            set { SetValue(ShowFullPathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ShowFullPath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShowFullPathProperty =
+            DependencyProperty.Register("ShowFullPath", typeof(bool), typeof(FileBox), new PropertyMetadata(true));
+
+        public string Filters
+        {
+            get { return (string)GetValue(FiltersProperty); }
+            set { SetValue(FiltersProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Filters.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FiltersProperty =
+            DependencyProperty.Register("Filters", typeof(string), typeof(FileBox), new PropertyMetadata("All Files|*.*"));
+
+        public bool DirectoryMode
+        {
+            get { return (bool)GetValue(DirectoryModeProperty); }
+            set { SetValue(DirectoryModeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DirectoryMode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DirectoryModeProperty =
+            DependencyProperty.Register("DirectoryMode", typeof(bool), typeof(FileBox), new PropertyMetadata(false));
+
+        public bool CheckExists
+        {
+            get { return (bool)GetValue(CheckExistsProperty); }
+            set { SetValue(CheckExistsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CheckExists.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CheckExistsProperty =
+            DependencyProperty.Register("CheckExists", typeof(bool), typeof(FileBox), new PropertyMetadata(true));
+
+        public event EventHandler<FunctionEventArgs<string>> OnChanged;
+
+        public FileBox()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick_Open(object sender, EventArgs e)
+        {
+            string path = string.Empty;
+
+            var pop = new CommonOpenFileDialog()
+            {
+                Title = Label,
+                InitialDirectory = Path,
+                IsFolderPicker = DirectoryMode
+            };
+
+            if (pop.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                path = pop.FileName;
+            }
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                Path = path;
+
+                UpdateGUI();
+                uiText.ClearValue(TextBox.ForegroundProperty);
+
+                OnChanged?.Invoke(this, new FunctionEventArgs<string>(Path));
+            }
+        }
+
+        private void OnInput(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if ((DirectoryMode && Directory.Exists(Path)) || !DirectoryMode && File.Exists(Path))
+                {
+                    UpdateGUI();
+                    OnChanged?.Invoke(this, new FunctionEventArgs<string>(Path));
+                }
+            }
+            else
+            {
+                Path = uiText.Text;
+
+                if (CheckExists)
+                {
+                    bool exists = DirectoryMode ? Directory.Exists(Path) : File.Exists(Path);
+                    if (exists)
+                        uiText.ClearValue(TextBox.ForegroundProperty);
+                    else
+                        uiText.Foreground = Brushes.Red;
+                }
+            }
+        }
+
+        public void UpdateGUI()
+        {
+            if (ShowFullPath)
+                uiText.Text = Path;
+            else if (DirectoryMode)
+                uiText.Text = new DirectoryInfo(Path).Name;
+            else
+                uiText.Text = System.IO.Path.GetFileName(Path);
+        }
+    }
+}
---
+++ HaLi.WPF/UI/LabeledUI.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HaLi.WPF.UI
+{
+    /// <summary>
+    /// Basic UI with a label
+    /// </summary>
+    public class LabeledUI : ContentControl
+    {
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(LabeledUI), new PropertyMetadata(string.Empty));
+
+        public Thickness LabelMargin
+        {
+            get { return (Thickness)GetValue(LabelMarginProperty); }
+            set { SetValue(LabelMarginProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelMargin.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelMarginProperty =
+            DependencyProperty.Register("LabelMargin", typeof(Thickness), typeof(LabeledUI), new PropertyMetadata(new Thickness(6,4,6,4)));
+
+
+        public Brush LabelBrush
+        {
+            get { return (Brush)GetValue(LabelBrushProperty); }
+            set { SetValue(LabelBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelBrushProperty =
+            DependencyProperty.Register("LabelBrush", typeof(Brush), typeof(LabeledUI), new PropertyMetadata(Brushes.Black));
+
+
+        public Dock LabelPlace
+        {
+            get { return (Dock)GetValue(LabelPlaceProperty); }
+            set { SetValue(LabelPlaceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelPlace.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelPlaceProperty =
+            DependencyProperty.Register("LabelPlace", typeof(Dock), typeof(LabeledUI), new PropertyMetadata(Dock.Top));
+
+
+        public HorizontalAlignment LabelHorizontalAlignment
+        {
+            get { return (HorizontalAlignment)GetValue(LabelHorizontalAlignmentProperty); }
+            set { SetValue(LabelHorizontalAlignmentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelHorizontalAlignment.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelHorizontalAlignmentProperty =
+            DependencyProperty.Register("LabelHorizontalAlignment", typeof(HorizontalAlignment), typeof(LabeledUI), new PropertyMetadata(HorizontalAlignment.Left));
+
+
+        public VerticalAlignment LabelVerticalAlignment
+        {
+            get { return (VerticalAlignment)GetValue(LabelVerticalAlignmentProperty); }
+            set { SetValue(LabelVerticalAlignmentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelVerticalAlignment.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelVerticalAlignmentProperty =
+            DependencyProperty.Register("LabelVerticalAlignment", typeof(VerticalAlignment), typeof(LabeledUI), new PropertyMetadata(VerticalAlignment.Center));
+
+
+        public double LabelWidth
+        {
+            get { return (double)GetValue(LabelWidthProperty); }
+            set { SetValue(LabelWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelWidthProperty =
+            DependencyProperty.Register("LabelWidth", typeof(double), typeof(LabeledUI), new PropertyMetadata(double.NaN));
+
+
+        public double LabelHeight
+        {
+            get { return (double)GetValue(LabelHeightProperty); }
+            set { SetValue(LabelHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelHeightProperty =
+            DependencyProperty.Register("LabelHeight", typeof(double), typeof(LabeledUI), new PropertyMetadata(double.NaN));
+
+
+        public bool MustFlag
+        {
+            get { return (bool)GetValue(MustFlagProperty); }
+            set { SetValue(MustFlagProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MustFlag.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MustFlagProperty =
+            DependencyProperty.Register("MustFlag", typeof(bool), typeof(LabeledUI), new PropertyMetadata(false));
+
+
+        public string MustSymbols
+        {
+            get { return (string)GetValue(MustSymbolsProperty); }
+            set { SetValue(MustSymbolsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MustSymbols.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MustSymbolsProperty =
+            DependencyProperty.Register("MustSymbols", typeof(string), typeof(LabeledUI), new PropertyMetadata("*"));
+
+
+        public Brush MustBrush
+        {
+            get { return (Brush)GetValue(MustBrushProperty); }
+            set { SetValue(MustBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MustBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MustBrushProperty =
+            DependencyProperty.Register("MustBrush", typeof(Brush), typeof(LabeledUI), new PropertyMetadata(Brushes.Red));
+
+
+
+
+
+        static LabeledUI()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LabeledUI), new FrameworkPropertyMetadata(typeof(LabeledUI)));
+        }
+    }
+}
