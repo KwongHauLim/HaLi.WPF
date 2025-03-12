120bcd8c85c2bafeb3a59101af392534ef7a12a1
Kwong Hau Lim <haulim.kwong@outlook.com>
2025-03-13 00:21:16 +0800

    GUI, Button

--- HaLi.WPF/GUI/Button.xaml.cs
+++ HaLi.WPF/GUI/Button.xaml.cs
@@ -1,66 +1,51 @@
-﻿using HaLi.WPF.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace HaLi.WPF.GUI
+namespace HaLi.WPF.GUI;
+
+/// <summary>
+/// Interaction logic for Button.xaml
+/// </summary>
+public partial class Button : UserControl
 {
-    /// <summary>
-    /// Interaction logic for Button.xaml
-    /// </summary>
-    public partial class Button : UserControl
+    public bool ShowLabel
     {
-        public bool ShowLabel
-        {
-            get { return (bool)GetValue(ShowLabelProperty); }
-            set { SetValue(ShowLabelProperty, value); }
-        }
+        get { return (bool)GetValue(ShowLabelProperty); }
+        set { SetValue(ShowLabelProperty, value); }
+    }
 
-        // Using a DependencyProperty as the backing store for ShowLabel.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ShowLabelProperty =
-            DependencyProperty.Register("ShowLabel", typeof(bool), typeof(Button), new PropertyMetadata(true, OnShowLabelChanged));
+    // Using a DependencyProperty as the backing store for ShowLabel.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty ShowLabelProperty =
+        DependencyProperty.Register("ShowLabel", typeof(bool), typeof(Button), new PropertyMetadata(true, OnShowLabelChanged));
 
-        private static void OnShowLabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    private static void OnShowLabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is Button uc)
         {
-            if (d is Button uc)
+            var label = uc.uiLabel;
+            if (uc.ShowLabel)
             {
-                uc.uiLabel.Visibility = uc.ShowLabel ? Visibility.Visible : Visibility.Collapsed;
-
-                if (uc.ShowLabel)
-                {
-                    uc.b.Width = double.NaN;
-                }
-                else
-                {
-                    uc.b.SetBinding(WidthProperty, GuiHelper.OneWay(uc.b, "ActualHeight"));
-                }
+                label.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                label.Visibility = Visibility.Collapsed;
             }
         }
+    }
 
-        public Button()
+    public Button()
+    {
+        InitializeComponent();
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        uiLabel.Visibility = ShowLabel ? Visibility.Visible : Visibility.Collapsed;
+
+        if (ShowLabel)
         {
-            InitializeComponent();
-        }
-
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            uiLabel.Visibility = ShowLabel ? Visibility.Visible : Visibility.Collapsed;
-
-            if (ShowLabel)
-            {
-                b.Width = double.NaN;
-            }
+            b.Width = double.NaN;
         }
     }
 }
---
+++ Sample/Panels/PanelButton.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample.Panels
+{
+    /// <summary>
+    /// Interaction logic for PanelButton.xaml
+    /// </summary>
+    public partial class PanelButton : UserControl
+    {
+        public PanelButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
