56606277cb25ea0a5b75c0b10ef192230dd1d64f
Kwong Hau Lim <haulim.kwong@outlook.com>
2025-03-23 12:47:31 +0800

    Tray menu with submenu

--- HaLi.WPF/Tray/Icon.xaml.cs
+++ HaLi.WPF/Tray/Icon.xaml.cs
@@ -15,6 +15,7 @@ namespace HaLi.WPF.Tray
         // double click event
         public event EventHandler MouseDoubleClick;
 
+        public Dictionary<string, MenuItem> MenuMap { get; private set; } = new();
 
         public Icon()
         {
@@ -33,51 +34,89 @@ namespace HaLi.WPF.Tray
             uiMenu.Items.Clear();
         }
 
-        public void AddSimple(string key, Action action)
+        private void AddTo(MenuItem? parent, MenuItem item)
+        {
+            if (parent == null)
+            {
+                uiMenu.Items.Add(item);
+            }
+            else
+            {
+                parent.Items.Add(item);
+            }
+
+            string key = GetKey(item);
+            MenuMap[key] = item;
+        }
+
+        private string GetKey(MenuItem item)
+        {
+            var key = string.Empty;
+            if (item.Header is string head)
+                key = head;
+
+            var parent = GetParent(item);
+            if (parent != null)
+            {
+                key = GetKey(parent) + "." + key;
+            }
+
+            return key;
+        }
+
+        private MenuItem GetParent(MenuItem menuItem)
+            => (menuItem.Parent is MenuItem parent) ? parent : null;
+
+        public MenuItem AddSimple(string key, Action action, MenuItem parent = null)
         {
             var item = new MenuItem();
             item.Header = key;
             item.Click += (s, e) => action();
-            uiMenu.Items.Add(item);
+            AddTo(parent, item);
+            return item;
         }
 
-        public void AddMenu(string key, Action action)
+        public MenuItem AddMenu(string key, Action action, MenuItem parent = null)
         {
             var item = new MenuItem();
             item.SetValue(MenuItem.HeaderProperty, key); // set header (key)
             item.Click += (s, e) => action();
-            uiMenu.Items.Add(item);
+            AddTo(parent, item);
+            return item;
         }
 
-        public void AddMenu(string key, object icon, Action action)
+        public MenuItem AddMenu(string key, object icon, Action action, MenuItem parent = null)
         {
             var item = new MenuItem();
             item.SetValue(MenuItem.HeaderProperty, key); // set header (key)
             item.Icon = icon;
             item.Click += (s, e) => action();
-            uiMenu.Items.Add(item);
+            AddTo(parent, item);
+            return item;
         }
 
-        public void AddMenu(string key, ICommand cmd)
+        public MenuItem AddMenu(string key, ICommand cmd, MenuItem parent = null)
         {
             var item = new MenuItem();
             item.SetValue(MenuItem.HeaderProperty, key); // set header (key)
             item.Click += (s, e) => cmd.Execute(s);
-            uiMenu.Items.Add(item);
+            AddTo(parent, item);
+            return item;
         }
 
-        public void AddMenu(string key, object icon, ICommand cmd)
+        public MenuItem AddMenu(string key, object icon, ICommand cmd, MenuItem parent = null)
         {
             var item = new MenuItem();
             item.SetValue(MenuItem.HeaderProperty, key); // set header (key)
             item.Icon = icon;
             item.Click += (s, e) => cmd.Execute(s);
-            uiMenu.Items.Add(item);
+            AddTo(parent, item);
+            return item;
         }
 
-        public void AddMenu(MenuItem item)
+        public void AddMenu(MenuItem item, MenuItem parent = null)
         {
-            uiMenu.Items.Add(item);
+            AddTo(parent, item);
         }
 
         public void AddMenuSeparator()
--- Sample/App.xaml.cs
+++ Sample/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using HaLi.WPF.Tray;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Sample
 {
@@ -9,6 +12,29 @@ namespace Sample
     /// </summary>
     public partial class App : Application
     {
+        internal static App _ptr;
+        internal Window? Showing { get; set; }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            _ptr = this;
+
+            Window win = new Window();
+            var Tray = new Icon();
+            Tray.SetIcon(GetUri("icon.png"));
+
+            var a = Tray.AddMenu("A", () => MessageBox.Show("A"));
+            var b = Tray.AddMenu("B", () => MessageBox.Show("B"), a);
+
+            win = NoWindow.Create(Tray);
+
+            Showing = new MainWindow();
+            Showing.Closed += (_, _) => Shutdown();
+            Showing.Show();
+
+            Uri GetUri(string key) => new Uri($"pack://application:,,,/{key}", UriKind.Absolute);
+            Image GetIcon(string key) => new Image { Source = new BitmapImage(GetUri(key)), Width = 16, Height = 16 };
+        }
     }
 
 }
